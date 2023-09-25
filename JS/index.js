67972cff50bd27fe493450f4bb84294acdadd090
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2023-09-25 17:04:12 +0600

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,12 @@
-console.log("CLicked");
+// console.log("CLicked");
+// let a=10;
+// let b=20;
+// console.log("Sumation is"+ (a+b));
+var a=10;
+while (a>=1) {
+    console.log(a);
+    a--;
+}
+do {
+    
+} while (condition); 
